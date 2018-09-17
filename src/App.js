2c93a08a9b9969f3dde07dc78138579127deed30
zachary toth <zacharytoth@zacharys-MacBook-Air.local>
2018-09-16 17:46:39 -0700

    edited app intro

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ class App extends Component {
           <h1 className="App-title">Welcome to AAA Apartment Staffing App!</h1>
         </header>
         <p className="App-intro">
-          AAA Apartment Staffing react app
+          AAA Apartment Staffing react mobile app
         </p>
       </div>
     );
