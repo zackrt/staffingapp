fc0977805e06abc8151dfd66f0b27415c8a12e3d
zachary toth <zacharytoth@zacharys-MacBook-Air.local>
2018-09-16 18:22:44 -0700

    added achinfo accountinfo notifications components

---
+++ src/components/achinfoPage.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class achinfoPage extends Component {
+  render() {
+    return (
+      <div>
+        <h1>ACH Info</h1>
+      </div>
+    )
+  }
+}
+
+export default achinfoPage
