01f16085baef815bf1d01d2c20b8b8dd39c786e6
zachary toth <zacharytoth@zacharys-MacBook-Air.local>
2018-09-16 22:17:49 -0700

    added EmailForm.js component

---
+++ src/components/EmailForm.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react'
+export class EmailForm extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {value: ''};
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({value: event.target.value});
+  }
+
+  handleSubmit(event) {
+    alert('an email address was submitted: ' + this.state.value);
+    event.preventDefault();
+  }
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <label>
+          Email:
+          <input type="text" value={this.state.value} onChange={this.handleChange} />
+        </label>
+        <input type="submit" value="Submit" />
+      </form>
+    );
+  }
+}
+export default EmailForm
--- src/components/loginPage.js
+++ src/components/loginPage.js
@@ -1,11 +1,13 @@
-import React, { Component } from 'react'
-
+import React, { Component } from 'react';
+import EmailForm from './EmailForm.js';
 export class loginPage extends Component {
   render() {
     return (
       <div>
-        <h1 className="LoginPageTitle">Login</h1>
+        <h1 className="login-page-title">Login</h1>
+
         <button className="settingsbtn">Settings</button>
+        <EmailForm/>
       </div>
     )
   }
