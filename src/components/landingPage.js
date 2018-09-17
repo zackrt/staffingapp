c8996a3e2d80211bab2b8b5e1a4a9a8c43781339
zachary toth <zacharytoth@zacharys-MacBook-Air.local>
2018-09-17 10:35:40 -0700

    added link import to landingPage

--- src/components/landingPage.js
+++ src/components/landingPage.js
@@ -1,4 +1,5 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import {Link} from 'react-router-dom';
 // Home Page or Landing Page component
 export class landingPage extends Component {
   render() {
