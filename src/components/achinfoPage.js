c661da45aa2c3c7b7bc0b2313eb7bc94f5312776
zachary toth <zacharytoth@zacharys-MacBook-Air.local>
2018-09-20 09:39:56 -0700

    text-align center to achinfopage

--- src/components/achinfoPage.js
+++ src/components/achinfoPage.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import './achinfoPage.css'
 // component for ach banking info page
 export class achinfoPage extends Component {
   render() {
@@ -6,9 +7,9 @@ export class achinfoPage extends Component {
       <div>
         <h1>ACH Info</h1>
         <h2 className="bankrouting-title">Bank Routing #</h2>
-        <input type="number"></input>
+        <input type="number" className="bankrouting_input" placeholder="023456789"></input>
         <h2 className="account-title">Account #</h2>
-        <input type="number"></input>
+        <input type="number"className="bankaccount_input" placeholder="543210123"></input>
       </div>
     )
   }
