237272c316cc8166fe3b154ab580d846f1a005b5
zachary toth <zacharytoth@zacharys-MacBook-Air.local>
2018-09-22 11:29:36 -0700

    changed layout to create account page

--- src/components/createaccountPage.js
+++ src/components/createaccountPage.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
+import './createaccountPage.css'
 export class createaccountPage extends Component {
   render() {
     return (
