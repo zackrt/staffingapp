b5cb19daec4aaa5aa6d5df8184fedd2e0938e73c
zachary toth <zacharytoth@zacharys-MacBook-Air.local>
2018-09-22 08:47:41 -0700

    adcentered inputs achpage

--- src/components/accountinfoPage.js
+++ src/components/accountinfoPage.js
@@ -8,8 +8,10 @@ export class accountinfoPage extends Component {
         <h1>Account Info </h1>
         <h2 className="fullname">Full Name</h2>
         <input className="fullname-text-input" type="text"placeholder="James Bond"></input>
-        <h3>Email</h3>
-        <h3>Password</h3>
+        <h3>Edit Email</h3>
+        <input className="edit-email-input" type="text" placeholder="email address"/>
+        <h3>Change Password</h3>
+        <input className="edit-password-input" type="password" placeholder="new password"/>
         <h4>Employee ID/Customer ID</h4>
         <h4>Type: Employee/Customer</h4>
       </div>
--- src/components/achinfoPage.js
+++ src/components/achinfoPage.js
@@ -4,12 +4,14 @@ import './achinfoPage.css'
 export class achinfoPage extends Component {
   render() {
     return (
-      <div>
+      <div className="achinfo_content">
         <h1>ACH Info</h1>
         <h2 className="bankrouting-title">Bank Routing #</h2>
         <input type="number" className="bankrouting_input" placeholder="023456789"></input>
         <h2 className="account-title">Account #</h2>
         <input type="number"className="bankaccount_input" placeholder="543210123"></input>
+        <br></br>
+        <button className="achinfo-save-btn btn btn-success">Save</button>
       </div>
     )
   }
