ad50669e649cec06932e31e58f317257cc2a0e65
zachary toth <zacharytoth@zacharys-MacBook-Air.local>
2018-09-22 11:25:31 -0700

    changed notifications input width

--- src/components/notificationsPage.js
+++ src/components/notificationsPage.js
@@ -5,24 +5,26 @@ export class notificationsPage extends Component {
     return (
       <div>
         <h1 className="notificationspage-title">Notifications</h1>
-        <div className="notificationspage-content input-group mb-3">
-          <div className="input-group-prepend">
-          <div className="input-group-text">
-          <input type="checkbox" aria-label="Checkbox for following text input" />
+        <div className="notificationspage-content">
+          <div className="input-group mb-3">
+            <div className="input-group-prepend">
+            <div className="input-group-text">
+            <input type="checkbox" aria-label="Checkbox for following text input" />
+          </div>
         </div>
-      </div>
           <input type="text" className="form-control" aria-label="Text input with checkbox" placeholder="When Jobs are Available" readOnly/>
-      </div>
+        </div>
           <br>
           </br>
           <div className="input-group mb-3">
-          <div className="input-group-prepend">
-          <div className="input-group-text">
-          <input type="checkbox" aria-label="Checkbox for following text input" />
-        </div>
-      </div>
-          <input type="text" className="form-control" aria-label="Text input with checkbox" placeholder="Messages from Recruiters" readOnly/>
-      </div>
+            <div className="input-group-prepend">
+              <div className="input-group-text">
+              <input type="checkbox" aria-label="Checkbox for following text input" />
+              </div>
+            </div>
+              <input type="text" className="form-control" aria-label="Text input with checkbox" placeholder="Messages from Recruiters" readOnly/>
+          </div>
+          </div>
       </div>
     )
   }
